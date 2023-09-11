4c93cef7df3a802a055c10914ee22bcd8c7eabd0
Dorigom <bluecloud7507@gmail.com>
2023-09-11 21:19:08 +0900

    feat : one 추가 , update 추가(수정중)fix : list, 컨트롤러 수정

--- src/main/java/com/game/team2/controller/UserInfoController.java
+++ src/main/java/com/game/team2/controller/UserInfoController.java
@@ -22,23 +22,64 @@ import lombok.extern.slf4j.Slf4j;
 public class UserInfoController {
     @Autowired
     private UserInfoService userService;
-    
-	@GetMapping("/user-infos")
-	public List<UserInfoVO> getUserInfos(UserInfoVO user){
 
-		return userService.getUserInfos(user);
-	}
-	@GetMapping("/user-infos/{uiNum}")
-	public UserInfoVO getUserInfo(@PathVariable int uiNum) {
-		log.info("uiNum=>{}", uiNum);
-		return userService.getUserInfo(uiNum);
-	}
+    @GetMapping("/user-infos")
+    public List<UserInfoVO> getUserInfos(UserInfoVO user) {
+
+        return userService.getUserInfos(user);
+    }
+
+    @GetMapping("/user-infos/{uiNum}")
+    public UserInfoVO getUserInfo(@PathVariable int uiNum) {
+        log.info("uiNum=>{}", uiNum);
+        return userService.getUserInfo(uiNum);
+    }
+
+    @PostMapping("/insertUserInfo")
+    public MsgVO insertUserInfo(@RequestBody UserInfoVO user) {
+        MsgVO msg = new MsgVO();
+        int result = userService.insertUserInfo(user);
+        if (result > 0) {
+            msg.setMsg("사용자 정보가 추가되었습니다.");
+            msg.setSuccess(true);
+        } else {
+            msg.setMsg("사용자 정보 추가에 실패했습니다.");
+        }
+        return msg;
+    }
+
+    @PostMapping("/updateUserInfo")
+    public MsgVO updateUserInfo(@RequestBody UserInfoVO user) {
+        MsgVO msg = new MsgVO();
+        int result = userService.updateUserInfo(user);
+        if (result > 0) {
+            msg.setMsg("사용자 정보가 수정되었습니다.");
+            msg.setSuccess(true);
+        } else {
+            msg.setMsg("사용자 정보 수정에 실패했습니다.");
+        }
+        return msg;
+    }
+
+    @PostMapping("/deleteUserInfo/{uiNum}")
+    public MsgVO deleteUserInfo(@PathVariable int uiNum) {
+        MsgVO msg = new MsgVO();
+        int result = userService.deleteUserInfo(uiNum);
+        if (result > 0) {
+            msg.setMsg("사용자 정보가 삭제되었습니다.");
+            msg.setSuccess(true);
+        } else {
+            msg.setMsg("사용자 정보 삭제에 실패했습니다.");
+        }
+        return msg;
+    }
+
     @PostMapping("/login")
-    public MsgVO login(@RequestBody UserInfoVO user, MsgVO msg, HttpSession session){
+    public MsgVO login(@RequestBody UserInfoVO user, MsgVO msg, HttpSession session) {
         log.info("user=>{}", user);
         UserInfoVO loginUser = userService.login(user);
         msg.setMsg("아이디나 비밀번호를 확인하세요");
-        if(loginUser!=null){
+        if (loginUser != null) {
             session.setAttribute("user", loginUser);
             msg.setMsg("로그인이 성공하였습니다.");
             msg.setUrl("/");
